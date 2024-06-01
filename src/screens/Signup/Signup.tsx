022d78b42eb32ce761ddbec8839d677c29ad5b91
Cyro MBLabs <cyroMBlabs@NMB-DESENV-421.local>
2024-05-31 23:53:04 -0300

    feat: signup update

--- src/screens/Signup/Signup.tsx
+++ src/screens/Signup/Signup.tsx
@@ -8,6 +8,24 @@ import { formatCellphone, isValidEmail, unmaskCpf } from '../../Utils/utils';
 import { CreateUserService } from '../../Services/CreateUserService';
 import { useNavigate } from 'react-router-dom';
 import { Loading } from '../../components/Loader/Loading';
+import logo from '../../assets/logo.svg';
+import eye from '../../assets/eye.svg';
+import eyeOff from '../../assets/eye-off.svg';
+import {
+	ButtonSignup,
+	Eye,
+	FieldInputValid,
+	FieldInputValidPass,
+	Link,
+	Logo,
+	LogoContainer,
+	PassContainer,
+	PassWrapper,
+	SignupField,
+	SignupMobileContainer,
+	SignupTitle,
+	SignupWrapper,
+} from './Signup.styles';
 
 const errorInitialValues = {
 	error_name: false,
@@ -30,6 +48,27 @@ export const Signup: React.FC = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [errorType, setErrorType] = useState<IErrorSignup>(errorInitialValues);
 	const cpf = sessionStorage.getItem('identifier') as string;
+
+	const [isEye, setIsEye] = useState<boolean>(false);
+	const [windowSize, setWindowSize] = useState({
+		width: window.innerWidth,
+		height: window.innerHeight,
+	});
+
+	useEffect(() => {
+		const handleResize = () => {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		};
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
 	useEffect(() => {
 		const validateName = () => name?.length > 3 && /\s/.test(name);
@@ -81,6 +120,9 @@ export const Signup: React.FC = () => {
 	const handleConfirmPassword = (password: string) => {
 		setConfirmPassword(password);
 	};
+	const handleEye = () => {
+		setIsEye(!isEye);
+	};
 	const handleClean = () => {
 		setPassword('');
 		setEmail('');
@@ -107,7 +149,7 @@ export const Signup: React.FC = () => {
 
 			if (response.status === 200) {
 				handleClean();
-				navigate('/login');
+				navigate('/Signup');
 			} else {
 				toast.error(response.message as string);
 			}
@@ -118,112 +160,261 @@ export const Signup: React.FC = () => {
 		}
 	};
 
+	const isMobile = windowSize.width <= 700;
 	return (
 		<>
 			{isLoading && <Loading />}
-			<div className="signup-container">
-				<div className="signup-block"></div>
-				<div className="signup-secound-block">
-					<div className="signup-field">
-						<div className="signup-wrapper-first">
-							<input
-								type="text"
-								value={name}
-								onChange={(e) => handleName(e.target.value)}
-								placeholder="Nome completo"
-								className={
-									!errorType.error_name
-										? 'field-input-valid'
-										: 'field-input-invalid'
-								}
-							/>
-							<span className="error-message">
-								{errorType.error_name && 'Digite o seu nome completo'}
-							</span>
-						</div>
+			{isMobile ? (
+				<SignupMobileContainer>
+					<div className="Signup-secound-block-Signup">
+						<SignupField>
+							<LogoContainer>
+								<Logo
+									src={logo}
+									alt="Logo Credit Black"
+								/>
+							</LogoContainer>
 
-						<div className="signup-wrapper">
-							<input
-								type="tel"
-								value={cellphone}
-								onChange={(e) => handleCellphone(e.target.value)}
-								placeholder="Celular"
-								maxLength={11}
-								className={
-									!errorType.error_cellphone
-										? 'field-input-valid'
-										: 'field-input-invalid'
-								}
-							/>
-							<span className="error-message">
-								{errorType.error_cellphone && 'Digite o número do seu celular'}
-							</span>
-						</div>
-						<div className="signup-wrapper">
-							<input
-								type="text"
-								value={email}
-								onChange={(e) => handleEmail(e.target.value)}
-								placeholder="Email"
-								className={
-									!errorType.error_email
-										? 'field-input-valid'
-										: 'field-input-invalid'
-								}
-							/>
-							<span className="error-message">
-								{errorType.error_email && 'Digite o seu email'}
-							</span>
-						</div>
-						<div className="signup-wrapper">
-							<input
-								type={hiddenPassword ? 'password' : 'text'}
-								value={password}
-								onChange={(e) => handlePassword(e.target.value)}
-								placeholder="Senha"
-								className={
-									!errorType.error_password
-										? 'field-input-valid'
-										: 'field-input-invalid'
-								}
-							/>
-							<span className="error-message">
-								{errorType.error_password && 'Mínimo 8 Caracteres'}
-							</span>
-							<span className="error-message">
-								{errorType.error_password &&
-									'letras, números e caracteres especiais'}
-							</span>
-						</div>
-						<div className="signup-wrapper">
-							<input
-								type={hiddenPassword ? 'password' : 'text'}
-								value={confirmPassword}
-								onChange={(e) => handleConfirmPassword(e.target.value)}
-								placeholder="Confirmação de Senha"
-								className={
-									!errorType.error_isSamePassword
-										? 'field-input-valid'
-										: 'field-input-invalid'
-								}
-							/>
-							<span className="error-message">
-								{errorType.error_password && 'As senhas precisam ser as iguais'}
-							</span>
-						</div>
-						{!isLoading && (
-							<button className={`button ${disabled ? 'disabled' : 'active'}`}>
-								<span
-									className="button-title"
-									onClick={handleContinue}
-								>
-									Continuar
+							<SignupTitle>Cadastre sua conta</SignupTitle>
+							<SignupWrapper>
+								<FieldInputValid
+									type="text"
+									value={name}
+									onChange={(e) => handleName(e.target.value)}
+									placeholder="Nome Completo"
+									maxLength={16}
+									className={
+										!errorType.error_name
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_name && 'Digite o seu nome completo'}
 								</span>
-							</button>
-						)}
+							</SignupWrapper>
+							<SignupWrapper>
+								<FieldInputValid
+									type="text"
+									value={cellphone}
+									onChange={(e) => handleCellphone(e.target.value)}
+									placeholder="Celular"
+									maxLength={16}
+									className={
+										!errorType.error_name
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_cellphone &&
+										'Digite o número do seu celular'}
+								</span>
+							</SignupWrapper>
+							<SignupWrapper>
+								<FieldInputValid
+									type="text"
+									value={email}
+									onChange={(e) => handleEmail(e.target.value)}
+									placeholder="Email"
+									className={
+										!errorType.error_name
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_email && 'Digite o seu email'}
+								</span>
+							</SignupWrapper>
+
+							<PassContainer>
+								<PassWrapper>
+									<FieldInputValidPass
+										type={!isEye ? 'password' : 'text'}
+										value={password}
+										onChange={(e) => handlePassword(e.target.value)}
+										placeholder="Senha"
+									/>
+									{!isEye ? (
+										<Eye
+											src={eyeOff}
+											alt="Esconder senha"
+											onClick={handleEye}
+										/>
+									) : (
+										<Eye
+											src={eye}
+											alt="Esconder senha"
+											onClick={handleEye}
+										/>
+									)}
+									<span className="error-message">
+										{errorType.error_password && 'Mínimo 8 Caracteres'}
+									</span>
+									<span className="error-message">
+										{errorType.error_password &&
+											'letras, números e caracteres especiais'}
+									</span>
+								</PassWrapper>
+							</PassContainer>
+							<PassContainer>
+								<PassWrapper>
+									<FieldInputValidPass
+										type={!isEye ? 'password' : 'text'}
+										value={confirmPassword}
+										onChange={(e) => handleConfirmPassword(e.target.value)}
+										placeholder="Confirmar Senha"
+									/>
+									{!isEye ? (
+										<Eye
+											src={eyeOff}
+											alt="Esconder senha"
+											onClick={handleEye}
+										/>
+									) : (
+										<Eye
+											src={eye}
+											alt="Esconder senha"
+											onClick={handleEye}
+										/>
+									)}
+									<span className="error-message">
+										{errorType.error_isSamePassword &&
+											'As senhas precisam ser as iguais'}
+									</span>
+								</PassWrapper>
+							</PassContainer>
+
+							{!isLoading && (
+								<>
+									<ButtonSignup
+										className={`button-Signup ${
+											disabled ? 'disabled' : 'active'
+										}`}
+										disabled={disabled}
+									>
+										<span
+											className="button-title"
+											onClick={handleContinue}
+										>
+											Continuar
+										</span>
+									</ButtonSignup>
+								</>
+							)}
+						</SignupField>
+					</div>
+				</SignupMobileContainer>
+			) : (
+				<div className="signup-container">
+					<div className="signup-block"></div>
+					<div className="signup-secound-block">
+						<div className="signup-field">
+							<div className="signup-wrapper-first">
+								<input
+									type="text"
+									value={name}
+									onChange={(e) => handleName(e.target.value)}
+									placeholder="Nome completo"
+									className={
+										!errorType.error_name
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_name && 'Digite o seu nome completo'}
+								</span>
+							</div>
+
+							<div className="signup-wrapper">
+								<input
+									type="tel"
+									value={cellphone}
+									onChange={(e) => handleCellphone(e.target.value)}
+									placeholder="Celular"
+									maxLength={11}
+									className={
+										!errorType.error_cellphone
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_cellphone &&
+										'Digite o número do seu celular'}
+								</span>
+							</div>
+							<div className="signup-wrapper">
+								<input
+									type="text"
+									value={email}
+									onChange={(e) => handleEmail(e.target.value)}
+									placeholder="Email"
+									className={
+										!errorType.error_email
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_email && 'Digite o seu email'}
+								</span>
+							</div>
+							<div className="signup-wrapper">
+								<input
+									type={hiddenPassword ? 'password' : 'text'}
+									value={password}
+									onChange={(e) => handlePassword(e.target.value)}
+									placeholder="Senha"
+									className={
+										!errorType.error_password
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_password && 'Mínimo 8 Caracteres'}
+								</span>
+								<span className="error-message">
+									{errorType.error_password &&
+										'letras, números e caracteres especiais'}
+								</span>
+							</div>
+							<div className="signup-wrapper">
+								<input
+									type={hiddenPassword ? 'password' : 'text'}
+									value={confirmPassword}
+									onChange={(e) => handleConfirmPassword(e.target.value)}
+									placeholder="Confirmação de Senha"
+									className={
+										!errorType.error_isSamePassword
+											? 'field-input-valid'
+											: 'field-input-invalid'
+									}
+								/>
+								<span className="error-message">
+									{errorType.error_isSamePassword &&
+										'As senhas precisam ser as iguais'}
+								</span>
+							</div>
+							{!isLoading && (
+								<button
+									className={`button ${disabled ? 'disabled' : 'active'}`}
+								>
+									<span
+										className="button-title"
+										onClick={handleContinue}
+									>
+										Continuar
+									</span>
+								</button>
+							)}
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
