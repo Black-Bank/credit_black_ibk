bd5f5f854863ca6065cee37346a593f5cc67de7b
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-05 20:52:20 -0300

    style(Changed media querie and added display flex for container in Home): :lipstick:

--- src/screens/Home/home.styles.tsx
+++ src/screens/Home/home.styles.tsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
+  display: flex;
   width: 85%;
   margin: 0 auto;
+
+  @media (max-width: 1200px) {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 `;
