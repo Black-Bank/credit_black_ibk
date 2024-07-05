f2ce0cb06c311863b2c1272432b4e265b5a3c156
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-05 17:36:45 -0300

    refactor(Create globalStyles from styled-components): :recycle:

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { AppRoutes } from './routes/routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
---
+++ src/styles/themes.styles.ts
@@ -0,0 +1,7 @@
+const themes = {
+  colors: {
+    primary: '#624AA7',
+  },
+};
+
+export default themes;
