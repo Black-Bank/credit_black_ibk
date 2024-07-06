885a0fdb4e991888edcb47178284b10f17bcd5f7
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-05 21:03:42 -0300

    refactor(Removed unnecessary code): :recycle:

--- src/components/Loader/loading.component.tsx
+++ src/components/Loader/loading.component.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import logo from '../../assets/logo.svg';
 import { LoadingContainer } from './loading.styles';
 
-export const Loading: React.FC = () => {
+export const Loading = () => {
   return (
     <LoadingContainer>
       <img src={logo} alt="Loading" className="loading-icon-loading" />
