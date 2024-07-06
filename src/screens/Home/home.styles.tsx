53b417c61295198fb3618743fefd22a974dce2eb
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-06 09:11:51 -0300

    style(Remove unnecessary code): :lipstick:

--- src/screens/Home/home.styles.tsx
+++ src/screens/Home/home.styles.tsx
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.main`
-  display: flex;
-  justify-content: space-between;
   width: 85%;
   margin: 0 auto;
-
-  @media (max-width: 1200px) {
-    flex-wrap: wrap;
-    justify-content: center;
-  }
 `;
