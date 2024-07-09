0d7bb34298ef22a11b03952b75f90066ad3eebba
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-09 12:51:32 -0300

    refactor(Added new colors for themes): :recycle:

--- src/styles/themes.styles.ts
+++ src/styles/themes.styles.ts
@@ -6,9 +6,11 @@ const themes = {
     gray1c1c1c: '#1C1C1C',
     grayb3b3b3: '#B3B3B3',
     gray1a1a1a: '#1A1A1A',
+    gray333333: '#333333',
     dark: '#1A1A1A',
     white: '#FFFFFF',
     black: '#000000',
+    error: '#D61A1F',
   },
 };
 
