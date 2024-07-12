c27194ab788f46bf0301ea6b8bdb77466ea817c6
Cyro MBLabs <cyroMBlabs@NMB-DESENV-421.local>
2024-07-12 10:32:36 -0300

    fix: removing Services

--- src/services/create-user-service.ts
+++
@@ -1,9 +0,0 @@
-import { ICreateUser } from './user.interface';
-import * as SignupAPI from '../api/signup';
-
-export class CreateUserService {
-  public createUser = async (user: ICreateUser) => {
-    const res = await SignupAPI.createUser(user);
-    return res;
-  };
-}
