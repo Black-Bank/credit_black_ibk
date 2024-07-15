41473743c4f92a1ea13c7eeb6d568c888c8a4b1b
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-15 13:42:46 -0300

    feat(Created private routes for Dashboard): :sparkles:

--- src/layouts/default.layout.tsx
+++ src/layouts/default.layout.tsx
@@ -1,13 +1,33 @@
 import MainDesign from 'components/Designs/MainDesign/main.design';
 import Header from 'components/Header/header.component';
 import { Container, MainContainer } from './default.styles';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { ScreenTypes } from 'components/Header/header.enum';
 import Footer from 'components/Footer/footer.component';
 import { RoutesEnum } from 'routes/routes.enum';
+import { useEffect } from 'react';
 
 const DefaultLayout = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    if (sessionStorage.getItem('accessToken')) {
+      if (
+        pathname === RoutesEnum.MAIN_ROUTE ||
+        pathname === RoutesEnum.LOGIN_ROUTE ||
+        pathname === RoutesEnum.SIGNUP_ROUTE
+      ) {
+        navigate(RoutesEnum.DASHBOARD_ROUTE);
+      }
+    } else if (!sessionStorage.getItem('accessToken')) {
+      if (pathname === RoutesEnum.DASHBOARD_ROUTE) {
+        navigate(RoutesEnum.MAIN_ROUTE);
+      }
+    }
+  });
 
   return (
     <>
