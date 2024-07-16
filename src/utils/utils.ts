5a51749e413102ef13d298d5087faa5e8034dc9c
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-16 18:00:37 -0300

    refactor(Removed utils codes unnecessary): :recycle:

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,25 +1,3 @@
-export const unmaskCpf = (cpf: string) => {
-  return cpf.replace(/[.-]/g, '');
-};
-
-export const isValidEmail = (email: string) => {
-  const atIndex = email.indexOf('@');
-  const dotComIndex = email.lastIndexOf('.com');
-
-  return atIndex !== -1 && dotComIndex !== -1 && dotComIndex > atIndex;
-};
-
-export const formatCellphone = (input: string) => {
-  const cleaned = ('' + input).replace(/\D/g, '');
-  const match = cleaned.match(/^(\d{2})(\d{1})(\d{4})(\d{4})$/);
-
-  if (match) {
-    return '(' + match[1] + ') ' + match[2] + ' ' + match[3] + '-' + match[4];
-  }
-
-  return input;
-};
-
 export const formatMoney = (value: number) => {
   return value.toLocaleString('pt-BR', {
     style: 'currency',
