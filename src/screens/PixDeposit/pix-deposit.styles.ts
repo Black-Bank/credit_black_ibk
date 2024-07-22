6671a38c2614c584912241918a902aab2253aee2
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-22 19:37:32 -0300

    feat(Created screen payment and information screen): :sparkles:

--- src/screens/PixDeposit/pix-deposit.styles.ts
+++ src/screens/PixDeposit/pix-deposit.styles.ts
@@ -28,10 +28,66 @@ export const Container = styled.div`
   input:focus {
     outline: 0;
   }
+
+  .error {
+    border-bottom: 1px solid ${themes.colors.error};
+  }
 `;
 
-export const Actions = styled.div`
+type ActionsProps = {
+  $buttontype: 'back' | 'next';
+};
+
+export const Actions = styled.div<ActionsProps>`
   display: flex;
-  justify-content: flex-end;
+  justify-content: ${(props) =>
+    props.$buttontype === 'back' ? 'space-between' : 'flex-end'};
   padding-top: 2rem;
+`;
+
+export const CodeContainer = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+
+  img {
+    border: 3px solid ${themes.colors.primary};
+  }
+`;
+
+export const KeyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+  border: 3px solid ${themes.colors.primary};
+  padding: 1rem;
+
+  button {
+    width: 70%;
+    margin-top: 1rem;
+    color: ${themes.colors.white};
+  }
+
+  @media (max-width: 600px) {
+    p {
+      font-size: 0.4rem;
+      width: 100%;
+    }
+  }
+
+  @media (min-width: 601px) and (max-width: 800px) {
+    p {
+      font-size: 0.5rem;
+      width: 100%;
+    }
+  }
+
+  @media (min-width: 801px) and (max-width: 1000px) {
+    p {
+      font-size: 0.7rem;
+      width: 100%;
+    }
+  }
 `;
