f57c2fb3f660fed9991cf2893f5dac7dd76fe987
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-07-31 18:02:11 -0300

    refactor(Created dashboard route): :recycle:

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -10,7 +10,7 @@ import Home from '../screens/Home/home.screen';
 import { Dashboard } from 'screens/Dashboard/dashboard.screen';
 import DefaultLayout from 'layouts/default.layout';
 import { RoutesEnum } from './routes.enum';
-import { PixDashboard } from 'screens/PixDashboard/pix-dashboard.screen';
+import Deposit from 'screens/Deposit/deposit.screen';
 
 export const AppRoutes = () => {
   return (
@@ -21,7 +21,7 @@ export const AppRoutes = () => {
           <Route path={RoutesEnum.LOGIN_ROUTE} element={<Login />} />
           <Route path={RoutesEnum.SIGNUP_ROUTE} element={<Signup />} />
           <Route path={RoutesEnum.DASHBOARD_ROUTE} element={<Dashboard />} />
-          <Route path={RoutesEnum.PIX_ROUTE} element={<PixDashboard />} />
+          <Route path={RoutesEnum.DEPOSIT_ROUTE} element={<Deposit />} />
           <Route
             path="*"
             element={<Navigate to={RoutesEnum.MAIN_ROUTE} replace />}
