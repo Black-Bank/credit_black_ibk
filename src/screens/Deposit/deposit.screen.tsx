43af4bfb5a4d6361f646c39e79460830b0870ad0
gaabssantos <gabriel.oliveira20500@gmail.com>
2024-08-01 15:05:13 -0300

    refactor(Finished deposit front): :recycle:

--- src/screens/Deposit/deposit.screen.tsx
+++ src/screens/Deposit/deposit.screen.tsx
@@ -1,5 +1,154 @@
+import { FaInfo } from 'react-icons/fa6';
+import {
+  Address,
+  AddressContainer,
+  AddressContent,
+  AddressSelect,
+  Container,
+  CurrentOption,
+  Options,
+  Orientation,
+  Orientations,
+  OrientationsContainer,
+  Payment,
+  Select,
+  SelectCoin,
+} from './deposit.styles';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import Dollar from '../../assets/icons/money-symbols/dollar.png';
+import { useEffect, useState } from 'react';
+import { getPix } from 'api/pix';
+import { CircleButton } from 'components/global.styles';
+
 const Deposit = () => {
-  return <h1>Hello, deposit!</h1>;
+  const [isOptionsVisible, setIsOptionsVisible] = useState(false);
+  const [base64, setBase64] = useState('');
+  const [pixKey, setPixKey] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { code, base64QRCode } = await getPix();
+
+      setBase64(base64QRCode);
+      setPixKey(code);
+    };
+
+    const createImageUrl = () => {
+      try {
+        if (base64.startsWith('data:image')) {
+          setImageUrl(base64);
+        } else {
+          const dataUrl = `data:image/png;base64,${base64}`;
+          setImageUrl(dataUrl);
+        }
+      } catch (err) {
+        console.error('Erro ao processar o base64:', err);
+      }
+    };
+
+    fetch();
+    createImageUrl();
+  });
+
+  return (
+    <Container>
+      <div>
+        <div>
+          <h6>Moeda selecionada</h6>
+          <SelectCoin $borderradius={isOptionsVisible}>
+            <Select onClick={() => setIsOptionsVisible(!isOptionsVisible)}>
+              <CurrentOption>
+                <img src={Dollar} alt="dólar" />
+                <p>Dólar</p>
+                <span>USD</span>
+              </CurrentOption>
+              {!isOptionsVisible ? (
+                <IoIosArrowDown className="select-arrow" />
+              ) : (
+                <IoIosArrowUp className="select-arrow" />
+              )}
+            </Select>
+            {isOptionsVisible && (
+              <Options>
+                <span>Nenhuma opção encontrada.</span>
+              </Options>
+            )}
+          </SelectCoin>
+        </div>
+        <div>
+          <h6>Orientações</h6>
+          <OrientationsContainer>
+            <Orientations>
+              <Orientation>
+                <FaInfo className="info" />
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati corporis voluptatum reiciendis delenitivoluptatum
+                  reiciendis delenitivoluptatum reiciendis deleniti
+                </p>
+              </Orientation>
+              <Orientation>
+                <FaInfo className="info" />
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati corporis voluptatum reiciendis delenitivoluptatum
+                  reiciendis delenitivoluptatum reiciendis deleniti
+                </p>
+              </Orientation>
+              <Orientation>
+                <FaInfo className="info" />
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati corporis voluptatum reiciendis delenitivoluptatum
+                  reiciendis delenitivoluptatum reiciendis deleniti
+                </p>
+              </Orientation>
+              <Orientation>
+                <FaInfo className="info" />
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Obcaecati corporis voluptatum reiciendis delenitivoluptatum
+                  reiciendis delenitivoluptatum reiciendis deleniti
+                </p>
+              </Orientation>
+            </Orientations>
+          </OrientationsContainer>
+        </div>
+      </div>
+      <AddressContainer>
+        <h6>Meu endereço</h6>
+        <Address>
+          <AddressContent>
+            <div>
+              <span>Saldo disponível</span>
+              <p className="value">R$ 0,00</p>
+            </div>
+            <div>
+              <span>Valor convertido</span>
+              <p className="converted-value">R$ 0,00</p>
+            </div>
+            <Orientation className="address-orientation">
+              <FaInfo className="info" />
+              <p>
+                Faça transferências apenas de contas de mesma titularidade da
+                sua conta CreditBlack
+              </p>
+            </Orientation>
+            <Payment>
+              <AddressSelect>
+                <span>PIX</span>
+              </AddressSelect>
+              <img src={imageUrl} alt="pix-qr-code" />
+              <p>{pixKey}</p>
+              <CircleButton>Copiar</CircleButton>
+              <CircleButton>Upload de comprovante</CircleButton>
+            </Payment>
+          </AddressContent>
+        </Address>
+      </AddressContainer>
+    </Container>
+  );
 };
 
 export default Deposit;
---
+++ src/screens/Deposit/deposit.styles.ts
@@ -0,0 +1,186 @@
+import styled, { css } from 'styled-components';
+import themes from 'styles/themes.styles';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 5fr 5fr;
+  grid-template-areas: 'left right';
+  gap: 1rem;
+  width: 80%;
+  margin: 0 auto;
+
+  h6 {
+    font-size: 1.2rem;
+    color: ${themes.colors.gray};
+    font-weight: normal;
+  }
+
+  svg.info {
+    font-size: 2rem;
+    color: ${themes.colors.information};
+  }
+
+  @media (max-width: 900px) {
+    display: block;
+    width: 100%;
+  }
+`;
+
+type SelectCoinProps = {
+  $borderradius: boolean;
+};
+
+export const SelectCoin = styled.div<SelectCoinProps>`
+  grid-area: left;
+  position: relative;
+  background-color: ${themes.colors.gray202022};
+  margin: 1rem 0;
+  ${(props) =>
+    props.$borderradius
+      ? css`
+          border-top-left-radius: 10px;
+          border-top-right-radius: 10px;
+        `
+      : css`
+          border-radius: 10px;
+        `}
+
+  select {
+    width: 100%;
+    background-color: ${themes.colors.gray202022};
+    padding: 1rem;
+    text-align: center;
+    color: ${themes.colors.white};
+    border: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    cursor: pointer;
+    font-size: 1rem;
+  }
+`;
+
+export const Select = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.5rem;
+  cursor: pointer;
+
+  .select-arrow {
+    position: absolute;
+    right: 0;
+    font-size: 1.5rem;
+    padding-right: 1rem;
+    cursor: pointer;
+  }
+
+  img {
+    width: 3rem;
+  }
+`;
+
+export const CurrentOption = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    font-size: 1.3rem;
+  }
+
+  span {
+    font-size: 0.7rem;
+    color: ${themes.colors.gray};
+    margin-left: 0.2rem;
+  }
+`;
+
+export const Options = styled.div`
+  position: absolute;
+  text-align: center;
+  padding: 2rem;
+  font-style: italic;
+  background-color: ${themes.colors.gray202022};
+  width: 84.5%;
+`;
+
+export const OrientationsContainer = styled.div`
+  grid-area: left;
+  background-color: ${themes.colors.gray202022};
+  margin: 1rem 0;
+  border-radius: 10px;
+`;
+
+export const Orientations = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  padding: 2rem 0;
+`;
+
+export const Orientation = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const AddressContainer = styled.div`
+  grid-area: right;
+`;
+
+export const Address = styled.div`
+  background-color: ${themes.colors.gray202022};
+  margin: 1rem 0;
+  border-radius: 10px;
+  height: 92%;
+`;
+
+export const AddressContent = styled.div`
+  width: 70%;
+  margin: 0 auto;
+  padding: 2rem 0;
+
+  span {
+    color: ${themes.colors.gray};
+  }
+
+  svg.info {
+    font-size: 1.2rem;
+  }
+
+  p.value,
+  p.converted-value {
+    font-size: 1.2rem;
+  }
+
+  .address-orientation {
+    border-top: 2px solid ${themes.colors.gray262626};
+    border-bottom: 2px solid ${themes.colors.gray262626};
+    margin: 0.8rem 0;
+    padding: 0.5rem 0;
+  }
+`;
+
+export const AddressSelect = styled.div`
+  span {
+    border-bottom: 1px solid ${themes.colors.primary};
+    font-size: 1.2rem;
+  }
+`;
+
+export const Payment = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 50%;
+    margin-top: 1rem;
+  }
+
+  p {
+    font-size: 0.6rem;
+    margin-top: 1rem;
+  }
+`;
